1f7b1022e097a5034ea546ae6b29ef70df7af9ce
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2023-12-11 14:47:34 +0100

    2023: Adds day 11 (part 1) in Python with input

---
+++ 2023/11.py
@@ -0,0 +1,77 @@
+""" Advent of Code 2023: Day 11 """
+
+FILE_TO_READ = "11_input"
+
+# - galaxies (#) and empty space (.)
+# - all rows and columns of only empty space need to be doubled
+# - then calculate the shortest path between galaxies
+
+import itertools
+
+
+def given():
+    with open(FILE_TO_READ, encoding="utf-8") as given_file:
+        lines_matrix = [list(l.rstrip("\n")) for l in given_file]
+    return lines_matrix
+
+
+# --- Part One --- #
+
+
+def print_space(image):
+    result = []
+    for row in image:
+        result.append("".join(row))
+    print("\n".join(result))
+
+
+def part_one():
+    space: list[list[str]] = given()
+
+    # 1) Expand space (double empty rows and columns)
+
+    # create copy to not change iterated sequence
+    new_image = []
+    for i, row in enumerate(space):
+        new_image.append(row)
+        if all(cell == "." for cell in row):
+            # copy row as otherwise the reference would point to the same list
+            new_image.append(row.copy())
+    space = new_image
+    # count cols in reverse so that insertion does not change the upcoming indices
+    for i in range(len(space[0]) - 1, -1, -1):
+        if all(row[i] == "." for row in space):
+            for row in space:
+                row.insert(i, ".")
+
+    # 2) Find all galaxies ("#")
+
+    galaxy_coordinates = []
+    for ri, row in enumerate(space):
+        for ci, cell in enumerate(row):
+            if cell == "#":
+                galaxy_coordinates.append((ri, ci))
+
+    # 3) Calculate shortest path between all pairs
+
+    sum_of_shortest_paths = 0
+    for left, right in itertools.combinations(galaxy_coordinates, 2):
+        lr, lc = left
+        rr, rc = right
+        sum_of_shortest_paths += abs(lr - rr) + abs(lc - rc)
+
+    return sum_of_shortest_paths
+
+
+# --- Part Two --- #
+
+
+def part_two():
+    return "NOT IMPLEMENTED"
+
+
+# --- Main Program --- #
+
+if __name__ == "__main__":
+    print(f"Part One: {part_one()}")
+    print(f"Part Two: {part_two()}")
