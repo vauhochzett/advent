d7f5a35230537543988c7563f0f0f80bb04f71f4
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2023-12-11 15:07:19 +0100

    2023: Adds day 11 (part 2) in Python

--- 2023/11.py
+++ 2023/11.py
@@ -71,9 +71,45 @@ def part_one():
 
 # --- Part Two --- #
 
+# Problem: Expansion is now be by a factor of 1M, so the naive algorithm is too slow.
+# Idea: Do not actually expand, instead just add "theoretical" steps to the path for
+#       each expansion.
+
 
 def part_two():
-    return "NOT IMPLEMENTED"
+    space: list[list[str]] = given()
+
+    # 1) Determine where we *would* expand
+
+    rows_to_expand = [
+        i for i, row in enumerate(space) if all(cell == "." for cell in row)
+    ]
+    cols_to_expand = [
+        i for i in range(len(space[0])) if all(row[i] == "." for row in space)
+    ]
+
+    # 2) Find all galaxies ("#")
+
+    galaxy_coordinates = galaxies(space)
+
+    # 3) Calculate shortest path between all pairs (with theoretical "expansion")
+
+    sum_of_shortest_paths = 0
+    for left, right in itertools.combinations(galaxy_coordinates, 2):
+        lr, lc = left
+        rr, rc = right
+        row_min, row_max = sorted([lr, rr])
+        col_min, col_max = sorted([lc, rc])
+        sum_of_shortest_paths += row_max - row_min + col_max - col_min
+        # skip our own row index, as it obviously contains a galaxy
+        for row_i in range(row_min + 1, row_max):
+            if row_i in rows_to_expand:
+                sum_of_shortest_paths += 999_999
+        for col_i in range(col_min + 1, col_max):
+            if col_i in cols_to_expand:
+                sum_of_shortest_paths += 999_999
+
+    return sum_of_shortest_paths
 
 
 # --- Main Program --- #
