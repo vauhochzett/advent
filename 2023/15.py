9aac362056ceb28640a941cea0af7f7c2ebc12c3
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2023-12-15 14:27:14 +0100

    2023: Adds day 15 (part 2) in Python

--- 2023/15.py
+++ 2023/15.py
@@ -9,6 +9,8 @@ FILE_TO_READ = "15_input"
 # - Set the current value to the remainder of dividing itself by 256
 
 
+from dataclasses import dataclass
+import re
 from typing import Iterable
 
 
@@ -42,8 +44,78 @@ def part_one():
 # --- Part Two --- #
 
 
+# HASHMAP sequence ;)
+
+# Manipulate lenses
+# - Set of 256 boxes, labeled 0–255
+# - Each operation targets box HASH(operation)
+# - xx=5 adds (or replaces) a lens labeled "xx" with focal strength 5
+# - xx- removes a lens labeled "xx" if it exists
+# - Lenses are added to the back of the list
+
+# Calculate focusing power
+# Multiply: (1 + box number) * (1-based slot number) * (focal length)
+
+
+@dataclass
+class Lens:
+    label: str
+    focal_length: int
+
+    def focusing_power(self, box_no: int, slot_no: int) -> int:
+        return (1 + box_no) * slot_no * self.focal_length
+
+
+class Box(list):  # list[Lens]
+    def focusing_power(self, box_no: int) -> int:
+        result: int = 0
+        for i, lens in enumerate(self, start=1):
+            result += lens.focusing_power(box_no, slot_no=i)
+        return result
+
+
+def split_operation(operation: str) -> tuple[str, str]:
+    match = re.fullmatch(r"(\w+)((?:-|=)\d*)", operation)
+    return match.group(1), match.group(2)
+
+
+def operate(box: Box, label: str, action: str) -> None:
+    """Perform `action` with lens `label` on `box`."""
+    index: int = -1
+    for i, lens in enumerate(box):
+        if lens.label == label:
+            index = i
+
+    if action == "-":
+        if index == -1:
+            return
+        box.pop(index)
+        return
+
+    assert action.startswith("=")
+    focal_length = int(action[1:])
+    if index == -1:
+        box.append(Lens(label, focal_length))
+        return
+    box[index].focal_length = focal_length
+
+
 def part_two():
-    return "NOT IMPLEMENTED"
+    boxes: dict[int, Box] = dict()
+    for number in range(256):
+        boxes[number] = Box()
+
+    # Perform lens changes on boxes
+    for value in given():
+        label, action = split_operation(value)
+        box_id = HASH(label)
+        operate(boxes[box_id], label, action)
+
+    # Calculate the focusing power
+    focusing_power: int = 0
+    for box_no, box in boxes.items():
+        focusing_power += box.focusing_power(box_no=box_no)
+    return focusing_power
 
 
 # --- Main Program --- #
