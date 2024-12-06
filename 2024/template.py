1c770efb882ffba4d5defd20fe653c4fdf29216f
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2024-12-06 17:46:07 +0100

    2024: Adds template

---
+++ 2024/template.py
@@ -0,0 +1,24 @@
+"""Advent of Code 2024: Day xx"""
+
+# --- Part One --- #
+
+
+def part_one(given):
+    raise NotImplementedError()
+
+
+# --- Part Two --- #
+
+
+def part_two(given):
+    raise NotImplementedError()
+
+
+# --- Main Program --- #
+
+
+with open("xx_input", encoding="utf-8") as fh:
+    given = [line.rstrip() for line in fh.readlines()]
+
+print(f"Part One: {part_one(given)}")
+print(f"Part Two: {part_two(given)}")
